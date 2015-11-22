3d7d86dc00c08bf7466a957b7c6ca5e0c933dc65
Shuhei Hiya <syuhei176@gmail.com>
2015-11-22 11:53:06 +0900

    Updated

--- jsmodal.js
+++ jsmodal.js
@@ -1,53 +1,100 @@
-(function() {
-	window.jsmodal = function(id) {
-		var elem = document.getElementById(id);
+(function(){
+	function Modal(options) {
+		var that = this;
+		this.options = options;
+		var elem = document.createElement('div');
 		var overlap = window.document.createElement("div");
-		overlap.style.display = "block";
-		overlap.style.opacity = "0.7";
-		overlap.style.position = "absolute";
-		overlap.style.width = "100%";
-		overlap.style.height = window.document.body.clientHeight + "px";
-		overlap.style.top = "0px";
-		overlap.style.left = "0px";
-		overlap.style.backgroundColor="#222";
-		return {
-			open : function(_option) {
-				var option = _option || {};
-				var self = this;
-				elem.style.display = 'block';
-				window.document.body.appendChild(overlap);
+		overlap.classList.add('jsmodal__screen');
+		this.overlap = overlap;
+		this.elem = elem;
+		this.elem.classList.add('jsmodal');
 
-				for(var i=0;i < elem.childNodes.length;i++) {
-					if(elem.childNodes[i].className == "close") {
-						console.log(elem.childNodes[i].className);
-						elem.childNodes[i].onclick = function() {
-							self.close();
-							return false;
-						}
-					}
+		var header = document.createElement('div');
+		var body = document.createElement('div');
+		var footer = document.createElement('div');
+		var buttons = document.createElement('div');
+		var okButton = document.createElement('button');
+		var cancelButton = document.createElement('button');
+
+		header.classList.add('jsmodal__header');
+		body.classList.add('jsmodal__body');
+		footer.classList.add('jsmodal__footer');
+		header.textContent = this.options || 'Untitled Modal';
+		cancelButton.textContent = 'Cancel';
+		okButton.textContent = 'OK';
+		cancelButton.classList.add('jsmodal__btn');
+		okButton.classList.add('jsmodal__btn');
+		okButton.classList.add('jsmodal__btn--positive');
+		buttons.classList.add('jsmodal__btnList');
+
+		this.body = body;
+		buttons.appendChild(cancelButton);
+		buttons.appendChild(okButton);
+		footer.appendChild(buttons);
+		this.elem.appendChild(header);
+		this.elem.appendChild(this.body);
+		this.elem.appendChild(footer);
+
+		window.document.body.appendChild(this.elem);
+
+		cancelButton.addEventListener('click', function() {
+			that.close();
+		});
+		okButton.addEventListener('click', function() {
+			that.okClicked();
+			that.close();
+		});
+	}
+
+	Modal.prototype.setBody = function(body) {
+		this.body.appendChild(body);
+	}
+	
+	Modal.prototype.ok = function(cb) {
+		this.okClicked = cb;
+	}
+
+	Modal.prototype.open = function(_option) {
+		var option = _option || {};
+		var self = this;
+		this.elem.style.display = 'block';
+		window.document.body.appendChild(this.overlap);
+
+		for(var i=0;i < this.elem.childNodes.length;i++) {
+			if(this.elem.childNodes[i].className == "close") {
+				this.elem.childNodes[i].onclick = function() {
+					self.close();
+					return false;
 				}
-				overlap.onmousedown = function(e) {
-					if(!check(e.target)) {
-						self.close();
-						return false;
-					}
-					function check(t, index) {
-						if(!t) return false;
-						if(index > 5) return false;
-						if(t.className == "jsmodal-window") {
-							return true;
-						}else{
-							return check(t.parentNode, index++);
-						}
-					}
-					return true;
-				}
-			},
-			close : function() {
-				elem.style.display = 'none';
-				if(overlap.remove) overlap.remove();
-				else window.document.body.removeChild(overlap);
 			}
 		}
+		this.overlap.onmousedown = function(e) {
+			if(!check(e.target)) {
+				self.close();
+				return false;
+			}
+			function check(t, index) {
+				if(!t) return false;
+				if(index > 5) return false;
+				if(t.className == "jsmodal") {
+					return true;
+				}else{
+					return check(t.parentNode, index++);
+				}
+			}
+			return true;
+		}
+
 	}
-}())
+
+	Modal.prototype.close = function() {
+		this.elem.style.display = 'none';
+		if(this.overlap.remove) this.overlap.remove();
+		else window.document.body.removeChild(this.overlap);
+	}
+	if(typeof exports === 'undefined') {
+		window.JSModal = Modal;
+	}else{
+		module.exports = Modal;
+	}
+}());
